b928b8acf6ff74befb417678e4d8014238782e4c
Dylon Gabriel Mandujano Gonzalez <94213859+Dylontech@users.noreply.github.com>
2025-06-17 07:47:45 -0600

    Add CreatesApplication trait and update homepage test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,26 @@
 
 namespace Tests;
 
+use Illuminate\Contracts\Console\Kernel;
+use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use CreatesApplication;
+}
+
+trait CreatesApplication
+{
+    /**
+     * Creates the application.
+     */
+    public function createApplication(): Application
+    {
+        $app = require __DIR__.'/../bootstrap/app.php';
+
+        $app->make(Kernel::class)->bootstrap();
+
+        return $app;
+    }
 }
